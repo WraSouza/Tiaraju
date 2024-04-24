cf99358a593f6f397ec32792dc079f28d5a383f9
Wladimir Ribeiro de Souza <wladimirsouza@outlook.com>
2024-04-24 09:47:31 -0300

    Adicionado a tela de manutenção. Precisa ser elaborada ainda.

---
+++ Tiaraju/ViewModels/DepartmentViewMode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiaraju.ViewModels
+{
+    partial class DepartmentViewMode : ObservableObject
+    {
+        [RelayCommand]
+        public async Task AbrirCalendarioCQ()
+        {
+            var route = $"{nameof(Views.CQMainView)}";
+            await Shell.Current.GoToAsync(route);
+        }
+
+        [RelayCommand]
+        public async Task AbrirManutencaoView()
+        {
+            var route = $"{nameof(Views.MAN.ManutencaoPrincipalView)}";
+            await Shell.Current.GoToAsync(route);
+        }
+    }
+}
